e9aefc8f6926aef67fd5f6a3e5199f207838e743
manitaKHO <manita.kho@gmail.com>
2018-06-19 10:40:30 +0700

    create Project

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MUwallet_Project.Startup))]
+namespace MUwallet_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
